c454fdf428376c4816463168e7036caf7981b428
azharkhan16 <azharkhan75447355@gmail.com>
2026-02-05 12:51:49 +0530

    Day1 -> Created producer and consumer application and basic setup using KafkaProducer and KafkaConsumer!

---
+++ consumer/src/main/java/com/ecommerce/consumer/KafkaConsumer.java
@@ -0,0 +1,33 @@
+package com.ecommerce.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaConsumer {
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group1")
+    public void listen1(String message) {
+        System.out.println("Received Message 1: " + message);
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-new-group2")
+    public void listen2(String message) {
+        System.out.println("Received Message 2: " + message);
+    }
+
+
+}
+
+
+//    Note:
+//    Condition1: same topic + same groupId and no od consumer = 2
+//    Consumer behaviour : Kafka message divide krega, Dono consumer load share krenge
+//                         Ek message sirf ek consumer ko milega , called -> Load Balancing
+//
+//    Note:
+//    Condition1: same topic + Different groupId and no od consumer = 2
+//    Consumer behaviour : Kafka message dono group ko send  krega, No load share
+//    Message Duplicate Consume hoga , called -> Publish subscribe Pattern
+//
+
